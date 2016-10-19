9593fe722a2b859bb543b637f5b065d1e6a7d217
Alexander Glassford <a.b.glassford@gmail.com>
2016-10-18 22:51:19 -0600

    nope

--- public/src/js/config.js
+++ public/src/js/config.js
@@ -9,12 +9,12 @@
   function appConfig($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'js/components/main/main.home.html',
+        templateUrl: 'public/src/js/components/main/main.home.html',
         controller: 'moviesController',
         controllerAs: 'moviesCtrl'
       })
       .when('/:movie', {
-        templateUrl: 'js/components/main/main.show.html',
+        templateUrl: 'public/src/js/components/main/main.show.html',
         controller: 'showController',
         controllerAs: 'showCtrl'
       })
---
+++ src/js/services/services.api.js
@@ -0,0 +1,33 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('api', [])
+    .service('accessApi', AccessApi)
+
+    AccessApi.$inject = ['$http']
+
+    function AccessApi ($http) {
+      this.movie = {}
+      this.callApi = (query) => {
+      return $http.get(`http://www.omdbapi.com/?s=${query}`)
+        .then((movieData) => {
+          this.movie = movieData
+        })
+        .catch((err) => {
+          throw err;
+        })
+      }
+      this.getMovieData = (movie) => {
+        return $http.get(`http://www.omdbapi.com/?t=${movie}&y=&plot=short&r=json`)
+        .then((movieData) => {
+          this.movie = movieData.data
+          console.log(this.movie);
+        })
+        .catch((err) => {
+          throw err;
+        })
+      }
+    }
+
+}());
